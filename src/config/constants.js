2f78c768566216acff05240fb3b8d9d3b1aec21c
jaussems <jannesaussems@gmail.com>
2021-05-27 19:43:27 +0200

    netlify deployment

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,5 @@
-export const apiUrl = process.env.API_URL;
+export const apiUrl =
+  process.env.API_URL || "https://see-crypto-app.netlify.app/";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 
 //export const apiUrl = process.env.API_URL || "http://localhost:4000";
