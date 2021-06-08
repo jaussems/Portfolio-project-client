1df47a3cb9da69ebebd0ecd3181c5022044c35a9
jaussems <jannesaussems@gmail.com>
2021-06-08 14:30:48 +0200

    added display gif

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,4 @@
-export const apiUrl =
-  process.env.API_URL || "https://see-crypto-app.netlify.app/";
+export const apiUrl = process.env.API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 
 //export const apiUrl = process.env.API_URL || "http://localhost:4000";
