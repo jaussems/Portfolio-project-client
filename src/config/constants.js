d99689e9897a11b7378acfe66a30fa1d6ced2552
jaussems <jannesaussems@gmail.com>
2021-06-11 19:14:25 +0200

    fixed deployment2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
-  process.env.apiUrl || "https://portfolio-crypto-server.herokuapp.com/";
+  process.env.apiUrl || "https://portfolio-crypto-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 
 //export const apiUrl = process.env.API_URL || "http://localhost:4000";
